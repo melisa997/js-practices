7e22f494db92b376ad5afc8166a5e6216ee9b6dc
LISA <82791851+melisa997@users.noreply.github.com>
2024-08-08 08:21:40 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,13 @@
+"use strict"; // treats all JS code as newer version
+
+let name = "LISA" //string
+let age = 27 //number
+let isLoggedIn = true //boolean
+let state; //undefined
+let a = null ; //null = standalone value
+//symbol unique
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
